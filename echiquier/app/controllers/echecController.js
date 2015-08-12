8fb4716f26e44d0fab878b84ca7a553ee4a2815c
unknown <learning1@PC0112.acensi.local>
2015-08-12 10:01:32 +0200

    ajout de commentaires dans le controller

--- echiquier/app/controllers/echecController.js
+++ echiquier/app/controllers/echecController.js
@@ -1,17 +1,17 @@
 app
     .controller('echecController', ['$scope', function($scope){
 
-        $scope.dimensions = [{},{},{},{},{},{},{},{}];
+        $scope.dimensions = [{},{},{},{},{},{},{},{}]; //dimensions de l'echiquier
+
         $scope.oddColor = '#ffffff';
         $scope.evenColor = '#000000';
 
-        $scope.addDimension = function(){
+        $scope.addDimension = function(){ // pour ajouter une ligne et une colonne à l'echiquier
             $scope.dimensions.push({});
         };
 
-        $scope.removeDimension = function(){
-            var lastElement = $scope.dimensions.length - 1;
-            $scope.dimensions.splice(lastElement, 1);
+        $scope.removeDimension = function(){ // pour retirer une ligne et une colonne à l'echiquier
+            $scope.dimensions.pop();
         };
 
     }]);
